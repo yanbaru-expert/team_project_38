cf6f929a8facdc7c34e4635cb719c9ef7249ae8a
KoyoIshikawa <koyoi561@gmail.com>
2020-12-28 23:53:01 +0900

    csvインポート機能を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,20 @@ User.find_or_create_by!(email: EMAIL) do |user|
   user.password = PASSWORD
   puts "ユーザーの初期データインポートに成功しました。"
 end
+class ImportCsv
+  Movie.destroy_all
+  def self.import(path)
+    list = []
+    CSV.foreach(path, headers: true) do |row|
+      list << row.to_h
+    end
+    list
+  end
+  def self.user_data
+    list = import('db/csv_data/movie_data.csv')
 
+    puts "インポート処理を開始"
+    Movie.create!(list)
+    puts "インポート完了!!"
+  end
+end
