73408144d671c6aaea3c78d757f53b1054a212e5
nmackee <mackee.base@gmail.com>
2021-01-02 23:32:58 +0900

    ユーザーデータを更新するための設定を修正

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register User do
-  permit_params :email
+  permit_params :email, :password, :password_confirmation
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -15,5 +15,5 @@ ActiveAdmin.register User do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
