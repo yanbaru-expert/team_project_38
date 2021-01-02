d691fab0d2213a4a2dff299b00fb3662e2ab78df
nmackee <mackee.base@gmail.com>
2021-01-02 23:52:46 +0900

    seedファイルを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
+# テストユーザーデータ
 EMAIL = "test@example.com"
 PASSWORD = "password"
+
+# 管理者ユーザーデータ
+ADMIN_EMAIL = "admin@example.com"
+ADMIN_PASSWORD = "password"
 
 # テストユーザーが存在しないときだけ作成
 User.find_or_create_by!(email: EMAIL) do |user|
@@ -10,9 +15,6 @@ Movie.destroy_all
 ImportCsv.user_data
 
 # 初期 管理者ユーザーデータを作成
-ADMIN_EMAIL = "admin@example.com"
-ADMIN_PASSWORD = "password"
-
 AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |adminuser|
   adminuser.password = ADMIN_PASSWORD
   adminuser.password_confirmation = ADMIN_PASSWORD
