735233bf565a2cded76007be0fda677649e2f68a
nmackee <mackee.base@gmail.com>
2021-01-02 20:51:33 +0900

    管理画面からデータを更新するための設定を追記

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Movie do
+  permit_params :genre, :title, :url
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
@@ -14,5 +15,5 @@ ActiveAdmin.register Movie do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
--- app/admin/texts.rb
+++ app/admin/texts.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Text do
+  permit_params :genre, :title, :content
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
