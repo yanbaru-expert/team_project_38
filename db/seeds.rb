5fbade2c266774fb99ddd277243fab5a74eb28e1
nmackee <mackee.base@gmail.com>
2021-01-02 20:09:18 +0900

    初期管理者ユーザーデータを作成するための処理を追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,13 @@ User.find_or_create_by!(email: EMAIL) do |user|
 end
 Movie.destroy_all
 ImportCsv.user_data
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# 初期 管理者ユーザーデータを作成
+ADMIN_EMAIL = "admin@example.com"
+ADMIN_PASSWORD = "password"
+
+AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |adminuser|
+  adminuser.password = ADMIN_PASSWORD
+  adminuser.password_confirmation = ADMIN_PASSWORD
+  puts "管理者ユーザーの初期データインポートに成功しました。"
+end
