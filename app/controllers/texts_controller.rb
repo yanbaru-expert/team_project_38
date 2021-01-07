ee6140abda602d52c9d7dcd19f61ba56aa7528cd
KoyoIshikawa <koyoi561@gmail.com>
2021-01-07 21:41:13 +0900

    showアクションを削除

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -4,9 +4,7 @@ class TextsController < ApplicationController
     @texts = Text.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]).order(id: :asc)
   end
 
-  def show
-    @text = Text.find(params[:id])
-  end
+  
   
 
 end
