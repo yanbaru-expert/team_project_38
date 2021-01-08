66033af4b41b62101334f06e09577936895552a6
Masato Kumatani <masato@MasatonoMacBook-Pro.local>
2021-01-08 23:22:10 +0900

    コントローラのフラッシュメッセージを削除

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -4,19 +4,5 @@ class TextsController < ApplicationController
     @texts = Text.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]).order(id: :asc)
   end
 
-  def create
-    text = Text.create!(post_params)
-    redirect_to post, notice: "投稿しました"
-  end
-
-  def update
-    @text.update!(post_params)
-    redirect_to @post, notice: "更新しました"
-  end
-
-  def destroy
-    @text.destroy!
-    redirect_to root_path, alert: "削除しました"
-  end
 
 end
