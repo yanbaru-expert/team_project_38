7cb851fbe12792d07331b2362004792c27cc060d
nmackee <mackee.base@gmail.com>
2021-01-11 18:41:40 +0900

    動画教材ページへのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root to: 'texts#index'
+  root to: "texts#index"
 
   resources :texts, only: [:index, :show]
+  resources :movies, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
