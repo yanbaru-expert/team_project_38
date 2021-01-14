599d7bf6180123ef41739e46e5a72204e71da66b
KoyoIshikawa <koyoi561@gmail.com>
2021-01-14 22:44:18 +0900

    helper.rbにログイン画面に遷移する際に利用するmax-widthメソッドがclass mw-smに条件分岐を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
-module ApplicationHelper
+module ApplicationHelper 
   def max_width
     # 後に条件分岐が必要
-    # max-width: var(--breakpoint-sm);
-    "mw-xl"
-    # もし、画面サイズが
-    # '768px'
+    if devise_controller?
+      "mw-sm"
+    end
+
   end
 end
