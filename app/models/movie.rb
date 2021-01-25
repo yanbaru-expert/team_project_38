9c603f100cb2a45723bad1cba69941ca8cc589e8
nmackee <mackee.base@gmail.com>
2021-01-25 16:09:43 +0900

    User,Movie,WatchedMovieモデルに関連付けとバリデーションを追記

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :watched_movies, dependent: :destroy
+
   validates :genre, presence: true
   validates :title, presence: true
   validates :url, presence: true
--- app/models/watched_movie.rb
+++ app/models/watched_movie.rb
@@ -1,4 +1,6 @@
 class WatchedMovie < ApplicationRecord
   belongs_to :user
   belongs_to :movie
+
+  validates :user_id, uniqueness: { scope: :movie_id }
 end
