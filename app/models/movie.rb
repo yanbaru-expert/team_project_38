a5e6fde65d40ad0735ec7bd12386a36c7e1938cb
nmackee <mackee.base@gmail.com>
2021-01-25 16:15:27 +0900

    視聴済みかどうかを判定するメソッドを作成

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,8 @@ class Movie < ApplicationRecord
   validates :genre, presence: true
   validates :title, presence: true
   validates :url, presence: true
+
+  def watched_by?(user)
+    watched_movies.find_by(user_id: user.id).present?
+  end
 end
