fa9d94b6dfc2d0d416f647001399d9a101519fb1
nmackee <mackee.base@gmail.com>
2021-01-25 19:32:53 +0900

    'WatchedMovieコントローラのアクションを編集

--- app/controllers/watched_movies_controller.rb
+++ app/controllers/watched_movies_controller.rb
@@ -1,11 +1,11 @@
 class WatchedMoviesController < ApplicationController
   def create
+    @movie = Movie.find(params[:movie_id])
     current_user.watched_movies.create!(movie_id: params[:movie_id])
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @movie = Movie.find(params[:movie_id])
     current_user.watched_movies.find_by(movie_id: params[:movie_id]).destroy!
-    redirect_back(fallback_location: root_path)
   end
 end
