472b15eda6757d9f838ff4b6c23ebdf04e18564d
KoyoIshikawa <koyoi561@gmail.com>
2021-01-26 16:41:17 +0900

    タイピングミスを修正、多：多の関連付けを実装

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,9 +1,10 @@
 class Text < ApplicationRecord
-  has_many :likes, dependent: :destroy
+  has_many :reads, dependent: :destroy
+  has_many :readd_users, through: :reads, source: :user
   validates :genre, presence: true
   validates :title, presence: true
   validates :content, presence: true
-
+  
   def read_by?(user)
     reads.find_by(user_id: user.id).present?
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_many :likes, dependent: :destroy
+  has_many :reads, dependent: :destroy
   # Include default devise modules. Others available are:
+  has_many :read_texts, through: :reads, source: :text
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
