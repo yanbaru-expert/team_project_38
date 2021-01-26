8fb088f47c4d9d0434727dae9f7fd0afd76ee757
KoyoIshikawa <koyoi561@gmail.com>
2021-01-26 15:54:33 +0900

    読了済みの条件分岐を導入

--- app/models/text.rb
+++ app/models/text.rb
@@ -3,4 +3,8 @@ class Text < ApplicationRecord
   validates :genre, presence: true
   validates :title, presence: true
   validates :content, presence: true
+
+  def read_by?(user)
+    reads.find_by(user_id: user.id).present?
+  end
 end
