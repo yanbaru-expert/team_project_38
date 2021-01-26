3cd558f1750b8ae83053344b96a57beb8ddf44ec
KoyoIshikawa <koyoi561@gmail.com>
2021-01-26 16:55:10 +0900

    readsコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: "texts#index"
 
   resources :texts, only: [:index, :show]
+    resource :reads, only: [:create, :destroy]
   resources :movies, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
