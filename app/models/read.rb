287525e4a05c432e2bbcfeb7d27bc797dd0afd1c
KoyoIshikawa <koyoi561@gmail.com>
2021-01-26 11:02:56 +0900

    user,text,readモデルにバリデーションと、関連付けを追加

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,4 +1,8 @@
 class Read < ApplicationRecord
   belongs_to :user
   belongs_to :text
+  validates :user_id, uniqueness: {
+    scope: :post_id,
+    message: "は同じ投稿に2回以上いいねはできません"
+  }
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,4 +1,5 @@
 class Text < ApplicationRecord
+  has_many :likes, dependent: :destroy
   validates :genre, presence: true
   validates :title, presence: true
   validates :content, presence: true
