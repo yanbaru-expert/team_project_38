c263535ebecb297eb71a6e5bceaf458a4b89a04a
KoyoIshikawa <koyoi561@gmail.com>
2021-01-28 16:29:19 +0900

    N+1問題を解消

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -2,6 +2,7 @@ class TextsController < ApplicationController
 
   def index
     @texts = Text.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]).order(id: :asc)
+    @read_text_ids = current_user.reads.pluck(:text_id)
   end
 
   def show
