ce5a44c9e3a915c8b486d33c56f2da336edd6fd8
KoyoIshikawa <koyoi561@gmail.com>
2021-01-29 00:16:28 +0900

    読破ボタンを非同期化

--- app/controllers/reads_controller.rb
+++ app/controllers/reads_controller.rb
@@ -1,12 +1,11 @@
 class ReadsController < ApplicationController
   def create
-    # binding.pry
+    @text = Text.find(params[:text_id])
     current_user.reads.create!(text_id: params[:text_id])
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @text = Text.find(params[:text_id])
     current_user.reads.find_by(text_id: params[:text_id]).destroy!
-    redirect_back(fallback_location: root_path)
   end
 end
