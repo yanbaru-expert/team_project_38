81b3667739620a2250982d6ed79ea158180d2550
Masato Kumatani <masato@MasatonoMacBook-Pro.local>
2021-02-01 22:23:38 +0900

    検索機能実装（コントローラー、ビュー）

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -2,6 +2,7 @@ class TextsController < ApplicationController
 
   def index
     @texts = Text.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]).order(id: :asc)
+    @q = Text.ransack(params[:q])
   end
 
   def show
