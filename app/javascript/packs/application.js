ad2e1a1b923b08f4280c2ec6130d793d11270fad
KoyoIshikawa <koyoi561@gmail.com>
2021-02-02 17:02:46 +0900

    Bootstrap と FontAwesome をインポート

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,4 @@ require("@rails/activestorage").start()
 require("channels")
 require("bootstrap/dist/js/bootstrap")
 import "bootstrap/dist/js/bootstrap"
+import "@fortawesome/fontawesome-free/js/all"
