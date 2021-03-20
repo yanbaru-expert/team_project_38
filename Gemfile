2583c7f4e11b0b9e46c9c8764fc6437b64b81509
satoumasato <masakun1993@gmail.com>
2021-03-20 22:47:43 +0900

    ページネーション実装の途中経過

--- Gemfile
+++ Gemfile
@@ -36,3 +36,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+# ページネーション機能
+gem 'kaminari'
+gem 'kaminari-bootstrap'
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,8 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]).order(id: :asc)
+    @movies = Movie.all.page(params[:page]).per(12)
+
     @watched_movie_ids = current_user.watched_movies.pluck(:movie_id)
   end
 end
