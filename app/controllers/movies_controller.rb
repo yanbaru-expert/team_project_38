78ee1f642ab84f96a743ad377ab5944cf14ceb05
satoumasato <masakun1993@gmail.com>
2021-03-29 20:08:34 +0900

    Movie_controllerのindexアクションの処理の修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.where(genre: ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]).order(id: :asc)
-    @movies = Movie.page(params[:page]).per(6)
+    genre_list = ["Basic", "Git", "HTML&CSS", "Ruby", "Ruby on Rails"]
+@movies = Movie.where(genre: genre_list).order(id: :asc).page(params[:page]).per(6)
 
     @watched_movie_ids = current_user.watched_movies.pluck(:movie_id)
   end
